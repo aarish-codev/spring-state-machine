83e83bf0d08b22626b18583b661b74091a575ead
AarishS <aarish.work@gmail.com>
2020-03-15 16:44:05 +0530

    added test cases and fixed code bugs

--- src/main/java/aarish/spring/statemachine/service/PaymentServiceImpl.java
+++ src/main/java/aarish/spring/statemachine/service/PaymentServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -22,31 +24,34 @@ public class PaymentServiceImpl implements PaymentService {
     private final PaymentStateChangeInterceptor paymentStateChangeInterceptor;
 
     @Override
+    @Transactional
     public Payment newPayment(Payment payment) {
         payment.setState(PaymentState.NEW);
         return paymentRepository.save(payment);
     }
 
     @Override
+    @Transactional
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
-
-        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHROIZE);
-        return null;
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_APPROVED);
+        return sm;
     }
 
     @Override
+    @Transactional
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         sendEvent(paymentId, sm, PaymentEvent.AUTH_DECLINED);
-        return null;
+        return sm;
     }
 
     @Override
+    @Transactional
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         sendEvent(paymentId, sm, PaymentEvent.AUTH_APPROVED);
-        return null;
+        return sm;
     }
 
     private void sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> sm, PaymentEvent event) {
